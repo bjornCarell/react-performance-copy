421f231f6bdd038fe0f9902688174fb05ea02e7c
Bjorn Carell <bjorn.carell@tink.com>
2021-09-02 08:55:15 +0200

    feat(exercise/1): Code Splitting

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,19 +2,25 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+// What webpack does, or the browser, or another bundler, is that it maintain
+// a cache for all promises made, and the resolved values, by dynamic imports.
+// So when React later goes ahead and loads the component the dynamic import
+// is already in the bundlers, or browsers, cache. 
+const loadGlobe = () => import('../globe')
+
+// The import is already in the bundlers, or browsers, cache, when this 
+// is getting rendered to the screen
+const Globe = React.lazy(loadGlobe)
+
+// If we are using webpack we can make use of Magic Comments. This comment will
+// should be used if we are just about 100% about what the next step the user 
+// is going to take will be. 
+// const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
@@ -26,7 +32,16 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      {/* 
+            We load the Globe component and it gets stashed to the bundlers, 
+            or browsers, cache. Giving the effect that the component 
+            loads faster to the screen.
+      */}
+      <label
+        style={{marginBottom: '1rem'}}
+        onMouseEnter={loadGlobe}
+        onFocus={loadGlobe}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
@@ -35,7 +50,9 @@ function App() {
         {' show globe'}
       </label>
       <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+        <React.Suspense fallback={<div>...loading</div>}>
+          {showGlobe ? <Globe /> : null}
+        </React.Suspense>
       </div>
     </div>
   )
